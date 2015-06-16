0c6b8fe7168da406e4ec2c60bed181cdeed7ba67
Tim Unkrig <tim@open-tim.org>
2015-06-16 17:37:41 +0200

    get httpResponse to work

--- app/src/main/java/htw_berlin/de/htwplus/MainActivity.java
+++ app/src/main/java/htw_berlin/de/htwplus/MainActivity.java
@@ -1,18 +1,47 @@
 package htw_berlin.de.htwplus;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import net.hamnaberg.json.Collection;
+import net.hamnaberg.json.parser.CollectionParser;
+import net.hamnaberg.funclite.Function;
+import net.hamnaberg.funclite.FunctionalList;
+import net.hamnaberg.funclite.Optional;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
 
 public class MainActivity extends Activity {
 
     boolean col = true;
+    private InputStream dum;
+    private String result = "";
+    private DefaultHttpClient client;
+    Collection collection;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        client = new DefaultHttpClient();
+        try {
+            collection = client.execute(new HttpGet(URI.create("http://localhost:9000/api/persons")), new CollectionResponseHandler());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(collection.toString());
     }
 
     @Override
@@ -36,4 +65,29 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private static class CollectionResponseHandler implements ResponseHandler<Collection> {
+        @Override
+        public Collection handleResponse(HttpResponse response) throws IOException {
+            if (response.getStatusLine().getStatusCode() == 200) {
+                if (isCollectionJSON(response)) {
+                    InputStream content = null;
+                    try {
+                        content = response.getEntity().getContent();
+                        return new CollectionParser().parse(content);
+                    } finally {
+                        if (content != null) {
+                            content.close();
+                        }
+                    }
+                }
+            }
+            throw new RuntimeException("No usable status here");
+        }
+
+        private boolean isCollectionJSON(HttpResponse response) {
+            return response.getEntity() != null && "application/vnd.collection+json".equals(response.getEntity().getContentType().getValue());
+        }
+    }
 }
+
