c3bd15b1c7833942eb46e6ce1d1e89825eb711e4
Tino Herrmann <s0542709@htw-berlin.de>
2015-07-17 17:44:14 +0200

    - ShowPostActivity erweitert: Auf einem Post-Beitrag kann nun geantwortet werden (Kommentar erstellen) + ButtonClickEventHandler hinzugefügt

--- app/src/main/java/htw_berlin/de/htwplus/view/ShowPostActivity.java
+++ app/src/main/java/htw_berlin/de/htwplus/view/ShowPostActivity.java
@@ -2,13 +2,19 @@ package htw_berlin.de.htwplus.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
+import net.hamnaberg.funclite.Optional;
 import net.hamnaberg.json.Collection;
+
+import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -24,24 +30,52 @@ import htw_berlin.de.htwplus.util.JsonCollectionHelper;
 
 public class ShowPostActivity extends Activity implements Response.Listener, Response.ErrorListener {
 
+    public static final String REQUEST_TAG = "ShowPostActivity";
     private int postId;
     private List<Post> mPostCommentList;
     private List<User> mUserList;
     private PostAdapter mPostAdapter;
     private ListView mCommentListview;
+    private EditText mCreateNewCommentEditText;
+    private Button mCreateNewCommentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_post);
+        mCommentListview = (ListView) findViewById(R.id.commentListView);
+        mCreateNewCommentEditText = (EditText) findViewById(R.id.createNewCommentEditText);
+        mCreateNewCommentButton = (Button) findViewById(R.id.createNewCommentButton);
         mPostCommentList = new ArrayList<Post>();
         mUserList = new ArrayList<User>();
         postId = getIntent().getExtras().getInt("postId");
         ApplicationController.getVolleyController().getUsers(this, this, this);
         ApplicationController.getVolleyController().getPostsFromNewsstream(this, this, this);
-        mCommentListview = (ListView) findViewById(R.id.commentListView);
         mPostAdapter = new PostAdapter(this, R.layout.post_listview_item_row, mPostCommentList, mUserList);
         mCommentListview.setAdapter(mPostAdapter);
+    }
+
+    public void onCreateNewCommentButtonClick(View v) {
+        try {
+            String commentMessage = mCreateNewCommentEditText.getText().toString();
+            if (!commentMessage.isEmpty()) {
+                mCreateNewCommentEditText.setText("");
+                ApplicationController.getVolleyController().addPost(commentMessage,
+                                                                    Optional.some(49l),
+                                                                    Optional.some(49l),
+                                                                    Optional.some(new Long(postId)),
+                                                                    null,
+                                                                    REQUEST_TAG,
+                                                                    this,
+                                                                    this);
+                ApplicationController.getVolleyController().getUsers(this, this, this);
+                ApplicationController.getVolleyController().getPostsFromNewsstream(this, this, this);
+            } else
+                Toast.makeText(getApplicationContext(), "Bitte Antwort eingeben!", Toast.LENGTH_LONG).show();
+        } catch (JSONException jex) {
+            Toast.makeText(getApplicationContext(), "JSON parse Exception!\nSiehe konsole!", Toast.LENGTH_LONG).show();
+            jex.printStackTrace();
+        }
     }
 
     @Override
