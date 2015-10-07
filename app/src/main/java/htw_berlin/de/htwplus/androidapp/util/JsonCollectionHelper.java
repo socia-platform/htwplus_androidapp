b7e0743a1fda413368b0a04b1f72acf998cd14ca
Tino Herrmann <s0542709@htw-berlin.de>
2015-10-07 21:29:19 +0200

    add documentation for the jsoncollectionhelper class

--- app/src/main/java/htw_berlin/de/htwplus/androidapp/util/JsonCollectionHelper.java
+++ app/src/main/java/htw_berlin/de/htwplus/androidapp/util/JsonCollectionHelper.java
@@ -20,11 +20,27 @@ import htw_berlin.de.htwplus.androidapp.datamodel.ApiError;
 import htw_berlin.de.htwplus.androidapp.datamodel.Post;
 import htw_berlin.de.htwplus.androidapp.datamodel.User;
 
+/**
+ * Represents a helper with useful functionality to parse and build Collection+JSON.
+ *
+ * @author Tino Herrmann, Tim Unkrig
+ * @version 1.0
+ */
 public class JsonCollectionHelper {
 
+    /**
+     * Constructor which is never called because it is static class.
+     */
     private JsonCollectionHelper() {
     }
 
+    /**
+     * Parses the given Collection+JSON for user data and converts them to a list of user-objects.
+     *
+     * @param collection Collection+JSON to be parsed.
+     *
+     * @return List of parsed and converted user-objects.
+     */
     public static List<User> toUsers(Collection collection) {
         List<User> users = new ArrayList<User>();
         for (Item item : collection.getItems()) {
@@ -51,6 +67,13 @@ public class JsonCollectionHelper {
         return users;
     }
 
+    /**
+     * Parses the given Collection+JSON for posting data and converts them to a list of
+     * posting-objects
+     *
+     * @param collection Collection+JSON to be parsed.
+     * @return List of parsed and converted posting-objects.
+     */
     public static List<Post> toPosts(Collection collection) {
         List<Post> posts = new ArrayList<Post>();
         for (Item item : collection.getItems()) {
@@ -89,6 +112,31 @@ public class JsonCollectionHelper {
         return posts;
     }
 
+    /**
+     * Builds a posting as Collection+JSON data with the given resource url, content, account id,
+     * owner id, parent id and group id. <br />
+     * <br />
+     * Should there be normal posting so account id and owner id must be the user id, who want
+     * to send, the other id's must be set to null. <br />
+     * Should there be a comment posting so account id and owner id must be the user id, who want
+     * to send, parent id must be the id of posting to which posting should be answered, group id
+     * must be null. <br />
+     * Should there be a group comment posting so account id and owner id must be the user id,
+     * who want to send, parent id must be the id of posting to which group posting should be
+     * answered, the group id must be the id of group in which the comment should be created.<br />
+     * <br />
+     * Example for resource url: <i>.../api/users/21</i> [optional] ?params such as access_token
+     *
+     * @param resourceUrl Resource url of the user, who want to send/create the posting
+     * @param content Content of new posting
+     * @param accountId Account id of new posting
+     * @param ownerId Owner id of new posting
+     * @param parentId Parent id of new posting
+     * @param groupId Group id of new posting
+     *
+     * @return Collection+JSON data with contained posting data.
+     * @throws JSONException Throws if the given params have invalid values.
+     */
     public static Collection buildPost(String resourceUrl, String content, Optional<Long> accountId,
                                        Optional<Long> ownerId, Optional<Long> parentId,
                                        Optional<Long> groupId) throws JSONException {
@@ -136,11 +184,26 @@ public class JsonCollectionHelper {
         return Collection.create(URI.create(resourceUrl), links, items, queries, template, null);
     }
 
+    /**
+     * Parses the given Collection+JSON for error data and converts them to a api error object.
+     *
+     * @param collection Collection+JSON to be parsed.
+     * @return Parsed and converted Api error object.
+     */
     public static ApiError toError(Collection collection) {
         net.hamnaberg.json.Error error = collection.getError().get();
         return new ApiError(error.getTitle(), error.getMessage(), error.getCode());
     }
 
+    /**
+     * Checks if the given data object contains the given property name. <br />
+     * The data object contains all data items of a of single Collection+JSON dataset.
+     *
+     * @param propertyName Proptery name
+     * @param data Data object
+     *
+     * @return True if the property name is contained otherwise false.
+     */
     private static boolean hasProperty(String propertyName, Data data) {
         boolean hasProperty = false;
         if (!data.propertyByName(propertyName).isNone())
@@ -148,6 +211,13 @@ public class JsonCollectionHelper {
         return hasProperty;
     }
 
+    /**
+     * Checks if the given Collection+JSON data contains errors. <br />
+     *
+     * @param collection Collection+JSON data
+     *
+     * @return True if an error is contained otherwise false.
+     */
     public static boolean hasError(Collection collection) {
         return (collection.hasError() && (collection.getError().get().getCode() != null) &&
                 (collection.getError().get().getMessage() != null) &&
